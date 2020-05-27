fffd342ab70c6f97b87fa947d52d54bc8a15e127
hieu-ng <hieutnguyen06@gmail.com>
2020-05-27 22:55:00 +1000

    Append transaction based on 'income' or 'expense'

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
+
+let transaction = undefined;
+
+function addTransactionDOM(transaction) {
+	const sign = transaction.amount < 0 ? '-' : '+';
+	const item = document.createElement('li')
+	item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+	item.innerHTML =
+		`
+		${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn"></button>`;
+	list.appendChild(item)
+}
