40672c98c481f6f7e4be60940e4ce66df0e7176b
hieu-ng <hieutnguyen06@gmail.com>
2020-05-27 23:08:50 +1000

    Add transaction based on DOM event

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ function addTransaction(e) {
 	}
 }
 
+function generateID() {
+	return Math.floor(Math.random() * 100000000);
+}
+
 function removeTransaction(id) {
 	transactions = transactions.filter(transaction => transaction.id !== id);
 
@@ -50,7 +54,10 @@ function addTransactionDOM(transaction) {
 	item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 	item.innerHTML =
 		`
-		${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn"></button>`;
+		${transaction.text} 
+		<span>${sign}${Math.abs(transaction.amount)}</span> 
+		<button class="delete-btn"></button>
+		`;
 	list.appendChild(item);
 }
 
